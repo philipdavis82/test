43ed2b78041b2699e8429730f8305101e6c195e1
Philip Davis <philip.davis.me@gmail.com>
2024-07-22 19:26:16 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float a = sqrtf(4);
+    float b = powf(a,2);
+    printf("Hello World\n");
+    printf("%f  %f\n",a,b);
+    return 0;
+}
